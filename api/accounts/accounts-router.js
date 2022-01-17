06347a3caa315160d8b98abfd58b2cb447b661de
david fletcher <davidfletcheraaron@gmail.com>
2022-01-17 11:46:05 -0500

    testing and wrote the error

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,7 +1,9 @@
 const router = require('express').Router()
 
 router.get('/', (req, res, next) => {
+  // SELECT * FROM Accounts;
   // DO YOUR MAGIC
+  return res.json('get wired')
 })
 
 router.get('/:id', (req, res, next) => {
@@ -22,6 +24,10 @@ router.delete('/:id', (req, res, next) => {
 
 router.use((err, req, res, next) => { // eslint-disable-line
   // DO YOUR MAGIC
+  res.status(err.status || 500).json({
+    message: err.message,
+    stack: err.stack
+  })
 })
 
 module.exports = router;
